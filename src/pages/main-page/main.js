f06581e2518053f397e0b92ae14d3e5c53356a50
TaoYu <121595113@qq.com>
2018-05-16 15:41:50 +0800

    echo 测试

--- src/pages/main-page/main.js
+++ src/pages/main-page/main.js
@@ -7,6 +7,14 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// add cordova.js only if serving the app through file://
+if (window.location.protocol === 'file:' || window.location.port === '3000') {
+  var cordovaScript = document.createElement('script')
+  cordovaScript.setAttribute('type', 'text/javascript')
+  cordovaScript.setAttribute('src', 'cordova.js')
+  document.body.appendChild(cordovaScript)
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
